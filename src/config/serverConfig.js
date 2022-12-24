8ec6a162862ccccc3ee56033882ff1d6c4c6d41f
vivekChenna <chennavivek96@gmail.com>
2022-12-23 17:47:43 -0700

    First Commit: basic skeleton Setup Done

---
+++ src/config/serverConfig.js
@@ -0,0 +1,10 @@
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+
+module.exports = {
+
+    PORT : process.env.PORT
+
+}
---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+
+const {PORT} = require('./config/serverConfig');
+
+const prepareAndStartServer = ()=>
+{
+
+    app.listen(PORT,()=>
+    {
+        console.log(`server started successfully on PORT ${PORT}`);
+    })
+
+}
+
+prepareAndStartServer();
+
