73c6d70c415f8de85677450638556f3af4143c31
vivekChenna <chennavivek96@gmail.com>
2022-12-25 23:42:17 -0700

    Implemented checkPassword function

--- src/index.js
+++ src/index.js
@@ -1,37 +1,35 @@
-const express = require("express");
+const express = require('express')
 
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser')
 
-const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken')
 
-const app = express();
+const app = express()
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+const { PORT, JWT_KEY } = require('./config/serverConfig')
 
-const { PORT, JWT_KEY } = require("./config/serverConfig");
+const v1ApiRoutes = require('./routes/index')
+// const UserService = require('./services/user-service')
 
-const v1ApiRoutes = require("./routes/index");
-const UserService = require("./services/user-service");
-
-app.use("/api", v1ApiRoutes);
+app.use('/api', v1ApiRoutes)
 
 const prepareAndStartServer = () => {
 
+  // const userService = new UserService()
 
-  const userService = new UserService();
+  // userService.createToken({email: 'vivekchenna@gmail.com',id: '1'})
 
-  userService.createToken({email:"vivekchenna@gmail.com",id:"1"});
-
-
-  const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InZpdmVrY2hlbm5hQGdtYWlsLmNvbSIsImlkIjoiMSIsImlhdCI6MTY3MjAxMjYxMX0.ASj78QZDQKveU5p60sqlkOMG14EPdearE3nbskMF2rI';
- const st =  jwt.verify(token,JWT_KEY);
- console.log(st);
+ 
+  // const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ.eyJlbWFpbCI6InZpdmVrY2hlbm5hQGdtYWlsLmNvbSIsImlkIjoiMSIsImlhdCI6MTY3MjAzNTU2MiwiZXhwIjoxNjcyMDM5MTYyfQ.cIach8qpZriAl0-z9DI49NlCMF_qftyHbVaas2hvZfg';
+  // const result = userService.verifyToken(token,JWT_KEY);
+  // console.log(result);
 
   app.listen(PORT, () => {
-    console.log(`server started successfully on PORT ${PORT}`);
-  });
-};
+    console.log(`server started successfully on PORT ${PORT}`)
+  })
+}
 
-prepareAndStartServer();
+prepareAndStartServer()
--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,7 +1,9 @@
 const UserRepository = require("../repository/user-repository");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
-const {JWT_KEY} = require('../config/serverConfig');
+const bcrypt = require('bcrypt');
+
+const { JWT_KEY } = require("../config/serverConfig");
 
 class UserService {
   constructor() {
@@ -28,41 +30,55 @@ class UserService {
     }
   }
 
-  async getUser(userId)
-  {
-    try {
 
+
+  async getUser(userId) {
+    try {
       const user = await this.userRepository.getUser(userId);
       return user;
-      
     } catch (error) {
-
-      console.log('something went wrong in the service layer');
-      throw {error};
-      
+      console.log("something went wrong in the service layer");
+      throw { error };
     }
   }
 
+  createToken(user) {
+    try {
+      const token = jwt.sign(user, JWT_KEY, { expiresIn: "1h" });
+      
+      console.log(token);
 
-  createToken(user)
-  {
 
-try {
-  
-  const token = jwt.sign(user,JWT_KEY);
-
-  console.log(token);
-  
-} catch (error) {
-
-  console.log('something went wrong in the token creation');
-  throw {error};
-  
-}
-
+      return token;
+    } catch (error) {
+      console.log("something went wrong in the token creation");
+      throw { error };
+    }
   }
 
+  verifyToken(token) {
+    try {
+      const response = jwt.verify(token, JWT_KEY);
 
+      return response;
+    } catch (error) {
+      console.log("something went wrong in the validation of token");
+      throw { error };
+    }
+  }
+
+  checkPassword(userInputPlainPassword,encryptedPassword)
+  {
+    try {
+
+      return bcrypt.compareSync(userInputPlainPassword,encryptedPassword);
+      
+      
+    } catch (error) {
+      console.log('something went wrong in password comparison');
+      throw {error};
+    }
+  }
 }
 
 module.exports = UserService;
